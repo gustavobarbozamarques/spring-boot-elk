44b1809fb1a46bb4342c355040ec06d3aa2002eb
gustavobarbozamarques <gustavomarques.contato@gmail.com>
2023-09-17 20:48:58 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spring Boot ELK
+
+This project demonstrates a simple microservice that uses Spring Boot, with ELK integration (Elasticsearch, Logstash and Kibana).
---
+++ src/main/java/br/com/gustavobarbozamarques/SpringBootElkApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavobarbozamarques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootElkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootElkApplication.class, args);
+	}
+
+}
